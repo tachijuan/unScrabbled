03001d60945d8ccbf7a3ba0c653c9cb6a768716e
Juan Orlandini <juan@orlandini.us>
2011-09-12 00:22:34 -0400

    Initial Commit

---
+++ unScrabbled/GIGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GIGAppDelegate.h
+//  unScrabbled
+//
+//  Created by Juan Orlandini on 9/12/11.
+//  Copyright (c) 2011 Gigantical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GIGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ unScrabbled/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  unScrabbled
+//
+//  Created by Juan Orlandini on 9/12/11.
+//  Copyright (c) 2011 Gigantical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GIGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GIGAppDelegate class]));
+    }
+}
