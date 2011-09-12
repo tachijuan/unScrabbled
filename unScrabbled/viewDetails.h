d3975a9f873d5c9d75c2fec0dcf716a2eb589200
Juan Orlandini <juan@orlandini.us>
2011-09-12 01:15:55 -0400

    Created Storyboard?

---
+++ unScrabbled/viewDetails.h
@@ -0,0 +1,13 @@
+//
+//  viewDetails.h
+//  unScrabbled
+//
+//  Created by Juan Orlandini on 9/12/11.
+//  Copyright (c) 2011 Gigantical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface viewDetails : UIViewController
+
+@end
