bfcc81f53ed4c450fc781bf94ae2b879775bb261
Juan Orlandini <juan@orlandini.us>
2011-09-16 23:04:44 -0400

    Added the help screen. Updated the read me to not have artificial breaks.

---
+++ unScrabbled/HelpViewer.h
@@ -0,0 +1,18 @@
+//
+//  HelpViewer.h
+//  unScrabbled
+//
+//  Created by Juan Orlandini on 9/16/11.
+//  Copyright (c) 2011 Gigantical. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewer : UIViewController {
+
+    IBOutlet UITextView *helpViewer;
+}
+
+@property (retain, nonatomic) IBOutlet UITextView *helpViewer;
+
+@end
